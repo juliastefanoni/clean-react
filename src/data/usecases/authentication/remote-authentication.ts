7bcd32aaeba9cf3ffa251a7f15d547ec07febc17
juliastefanoni <juliastefanonidev@gmail.com>
2020-12-07 15:17:45 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,19 +2,33 @@ import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AccountModel } from '@/domain/models/account-model'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication {
-  constructor (private readonly url: string, private readonly HttpPostClient: HttpPostClient) {}
+  constructor (
+    private readonly url: string,
+    private readonly HttpPostClient: HttpPostClient<
+    AuthenticationParams,
+    AccountModel
+    >
+  ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
-    const httpResponse = await this.HttpPostClient.post({ url: this.url, body: params })
+    const httpResponse = await this.HttpPostClient.post({
+      url: this.url,
+      body: params
+    })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
-      case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-      case HttpStatusCode.badRequest: throw new UnexpectedError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.ok:
+        break
+      case HttpStatusCode.unathorized:
+        throw new InvalidCredentialsError()
+      case HttpStatusCode.badRequest:
+        throw new UnexpectedError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
