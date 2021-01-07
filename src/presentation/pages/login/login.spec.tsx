fc46a0ad1ebbd498f9eb5f2af7b25adbe0b8dc9f
tiago.bordim.acc <tiago.bordim@accurate.com.br>
2021-01-07 14:58:57 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -60,4 +60,15 @@ describe('Testing Login Component', () => {
       email: 'any_email'
     })
   })
+
+  test('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
