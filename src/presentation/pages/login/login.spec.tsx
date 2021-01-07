c236ec5f7edd66c3b4dfa386516123c2f46e35b0
tiago.bordim.acc <tiago.bordim@accurate.com.br>
2021-01-07 14:31:36 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,10 +6,21 @@ import Login from './login'
 describe('Testing Login Component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
+
     const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
+
     const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
 
     expect(errorWrap.childElementCount).toBe(0)
-    expect(submitButton.disabled).toBe(true)
+
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
